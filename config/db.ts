481b2ca17791609b26187983b362d92459acc643
Abdullah Alqahtani <0xqahta0@gmail.com>
2023-06-03 23:21:28 +0300

    fix: add alternative MOGOURI url

--- config/db.ts
+++ config/db.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const mongoDBConnection = async (): Promise<void> => {
     try {
-        const mongoURI: string = process.env.MONGO_URI || "";
+        const mongoURI: string = process.env.MONGO_URI || "mongodb://abdullah:abdullah@mongodb:27017";
         await mongoose.connect(mongoURI);
         console.log('MongoDB connected successfully');
     } catch (error) {
